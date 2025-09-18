e610f38ce0b384d0a7907f7cd899fed9c57bf670
Quest-play <82810144+Quest-play@users.noreply.github.com>
2025-09-18 22:01:03 +0300

    Convert ItemRegisterer to primary constructor and add item list

--- Content/ItemRegisterer.cs
+++ Content/ItemRegisterer.cs
@@ -1,20 +1,21 @@
+using System.Collections.Generic;
 using GKAPI.Items;
 
 namespace MoreItems.Content;
 
 
-class ItemRegisterer
+class ItemRegisterer(ItemAPI api)
 {
-    private ItemAPI itemAPI;
-
-    public ItemRegisterer(ItemAPI itemAPI)
-    {
-        this.itemAPI = itemAPI;
-    }
-
+    private ItemAPI itemAPI = api;
+    private List<GkItem> ItemList { get; set; } = [];
 
     public void Register()
-    { 
+    {
         
+    }
+
+    public List<GkItem> getItemList()
+    {
+        return ItemList;
     }
 }
